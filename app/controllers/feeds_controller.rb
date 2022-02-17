64389d3ad14f644038a49799c67949fad4b507f2
shouhei-yamamoto <elefish19920509@gmail.com>
2022-02-17 14:19:40 +0900

    画像投稿確認画面の実装

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,16 +1,13 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: %i[ show edit update destroy ]
 
-  # GET /feeds or /feeds.json
   def index
     @feeds = Feed.all
   end
 
-  # GET /feeds/1 or /feeds/1.json
   def show
   end
 
-  # GET /feeds/new
   def new
     if params[:back]
       @feed = Feed.new(feed_params)
@@ -23,12 +20,10 @@ class FeedsController < ApplicationController
     @feed = Feed.new(feed_params)
     @feed.user_id = current_user.id
   end
-
-  # GET /feeds/1/edit
+ 
   def edit
   end
-
-  # POST /feeds or /feeds.json
+  
   def create
     @feed = Feed.new(feed_params)
     @feed.user_id = current_user.id
@@ -43,7 +38,7 @@ class FeedsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /feeds/1 or /feeds/1.json
+  
   def update
     respond_to do |format|
       if @feed.update(feed_params)
@@ -56,7 +51,6 @@ class FeedsController < ApplicationController
     end
   end
 
-  # DELETE /feeds/1 or /feeds/1.json
   def destroy
     @feed.destroy
     respond_to do |format|
@@ -66,13 +60,12 @@ class FeedsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_feed
-      @feed = Feed.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def feed_params
-      params.require(:feed).permit(:image, :content, :image_cache, :user_id)
-    end
+  def set_feed
+    @feed = Feed.find(params[:id])
+  end
+
+  def feed_params
+    params.require(:feed).permit(:image, :content, :image_cache, :user_id)
+  end
 end
